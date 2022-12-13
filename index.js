ab0ae97878cc61687f12bc6f7bedd526cc8208b0
Joshua Bays <68129596+bays2023@users.noreply.github.com>
2022-12-13 15:39:43 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
-let generate_random_number = new Math.seedrandom('josh');
+//let seed = prompt('Please enter in a seed for the RNG')
+let generate_random_number = new Math.seedrandom(seed);
 
 function randInt(low, high){
 	var ret = Math.abs(generate_random_number.int32()) % high;
@@ -6,5 +7,11 @@ function randInt(low, high){
 	return ret;
 }
 
-alert(randInt(1,10));
-alert(randInt(1,10));
+function submitMaterial(){
+	var choiceRange = document.getElementById("material-selector").value;
+	alert(choiceRange);
+	document.getElementByID("submitMaterial").style.visibility = 'hidden';
+}
+
+//alert(randInt(1,10));
+//alert(randInt(1,10));
