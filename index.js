76c503ef4e9c5fd0f6639b8d4dc470ebcfaffb15
Joshua Bays <68129596+bays2023@users.noreply.github.com>
2022-12-13 21:07:36 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,5 +1,10 @@
 //let seed = prompt('Please enter in a seed for the RNG')
+seed = 'josh';
 let generate_random_number = new Math.seedrandom(seed);
+var verseList = [];
+var index = 0;
+var verse = "";
+var showingVerse = 0;
 
 function randInt(low, high){
 	var ret = Math.abs(generate_random_number.int32()) % high;
@@ -7,10 +12,38 @@ function randInt(low, high){
 	return ret;
 }
 
-function submitMaterial(){
-	var choiceRange = document.getElementById("material-selector").value;
-	alert(choiceRange);
-	document.getElementByID("submitMaterial").style.visibility = 'hidden';
+function getMaterial(){
+	var choiceRange = document.getElementById("material-selector").value.slice(1);
+	document.getElementById("material-selector").style.visibility = 'hidden';
+	document.getElementById("quoteBtn").style.visibility = 'hidden';
+	document.getElementById("bequotedBtn").style.visibility = 'hidden';
+	document.getElementById("verseCorrect").style.visibility = 'visible';
+	document.getElementById("verseIncorrect").style.visibility = 'visible';
+	var refMin = parseInt(choiceRange.split(",")[0]) - 1;
+	var refMax = parseInt(choiceRange.split(",")[1]) - 1;
+	for(var i = refMin; i <= refMax; i++){ verseList.unshift(masterList[i]);}
+}
+
+function quoteBtn(showVerse){
+	showingVerse = showVerse; getMaterial();
+	if(showingVerse){ document.getElementById("verse").style.visibility = 'visible'; }
+	index = randInt(0, verseList.length);
+	document.getElementById("title").innerHTML = verseList[index][1];
+	if(showingVerse){ document.getElementById("verse").innerHTML = verseList[index][2]; }
+}
+
+function verseResult(correct){
+	if(correct){
+		verseList.splice(index, 1);
+		if(verseList.length == 0){
+			document.getElementById("title").innerHTML = "You are finished!";
+			document.getElementById("verseCorrect").style.visibility = 'hidden';
+			document.getElementById("verseIncorrect").style.visibility = 'hidden';
+		}
+	}
+	index = randInt(0, verseList.length);
+	document.getElementById("title").innerHTML = verseList[index][1];
+	if(showingVerse){ document.getElementById("verse").innerHTML = verseList[index][2]; }
 }
 
 //alert(randInt(1,10));
